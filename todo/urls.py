dc47671f6cacf27ba471d702e9101e3862da6321
Hsieh <max870701@gmail.com>
2024-01-06 04:15:17 -0800

    Complete Edit Task functionality

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
     path('markAsDone/<int:pk>', views.markAsDone, name='markAsDone'),
+    path('markAsUndone/<int:pk>', views.markAsUndone, name='markAsUndone'),
+    path('editTask/<int:pk>', views.editTask, name='editTask'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from .models import Task
 
 def addTask(request):
@@ -11,3 +11,22 @@ def markAsDone(request, pk):
     task.is_completed = True
     task.save()
     return redirect('home')
+
+def markAsUndone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def editTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        task.task = new_task
+        task.save()
+        return redirect('home')
+    else:
+        context = {
+            'task': task
+        }
+        return render(request, 'editTask.html', context)
